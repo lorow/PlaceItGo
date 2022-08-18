8d5cab4d4d7c757c8662683cb76994333aaa7fc9
Lorow <smykupyka@gmail.com>
2022-08-18 22:55:21 +0200

    improve logging

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,29 @@
 package main
 
 import (
+	"io"
+	"log"
+	"os"
+
 	pkg "github.com/lorow/placeitgo/internal"
-	log "github.com/sirupsen/logrus"
 )
 
-func main() {
-	mainLogger := log.New()
-	logger := log.NewEntry(mainLogger)
-
-	err := pkg.StartServer()
-
+func setupLogging() {
+	logFile, err := os.OpenFile("log.txt", os.O_CREATE | os.O_APPEND | os.O_RDWR, 0666)
 	if err != nil {
+			panic(err)
+	}
+	mw := io.MultiWriter(os.Stdout, logFile)
+	log.SetOutput(mw)
+}
+
+func main() {
+	setupLogging()
+
+	server_err := pkg.StartServer()
+
+	if server_err != nil {
 		panic("couldn't start the API server up")
 	}
-	logger.Info("Started serving")
+
 }
