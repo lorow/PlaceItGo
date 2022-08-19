5766e4f03e7ed83e9e664606b31a36189bff3aed
lorow <goik.zdzislaw@gmail.com>
2022-08-19 23:16:20 +0000

    add selecting random cached image from redis

--- internal/cache.go
+++ internal/cache.go
@@ -2,6 +2,9 @@ package internal
 
 import (
 	"context"
+	"fmt"
+	"math/rand"
+	"strings"
 
 	"github.com/go-redis/redis/v8"
 )
@@ -17,13 +20,31 @@ type RedisCache struct {
 	db *redis.Client
 }
 
+func (r RedisCache) getImageKeys(resolution string) string {
+	keys := make([]string, 5)
+
+	for i := 0; i < len(keys); i++ {
+		keys[i] = fmt.Sprintf("%s_%d", resolution, i)
+	}
+
+	return strings.Join(keys, " ")
+}
+
 func (r RedisCache) GetImages(resolution string) ([][]byte, error) {
-	// todo implment searching for multiple keys matching 
-	image, err := r.db.Get(ctx, resolution).Result()
+	keys := r.getImageKeys(resolution)
+
+	images, err := r.db.MGet(ctx, keys).Result()
+
+	if err != nil {
+		return nil, err
+	}
+	randomIndex := rand.Intn(len(images))
+	image := fmt.Sprint(images[randomIndex])
+
 	return [][]byte{[]byte(image)}, err
 }
 
-func GetRedisCache(cfg Config) (*RedisCache) {
+func GetRedisCache(cfg Config) *RedisCache {
 	rdb := redis.NewClient(&redis.Options{
 		Addr:     cfg.redis_url,
 		Password: cfg.redis_password,
